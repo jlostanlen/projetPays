aeecd4e0464fdb080fb0c72fbbe1f853ace2746c
jlostanlen <jeremie.lostanlen@gmail.com>
2023-03-07 15:34:13 +0100

    Class language

---
+++ js/Language.js
@@ -0,0 +1,12 @@
+class Language{
+    constructor(iso639_2, name){
+        this.iso639_2 = iso639_2
+        this.name = name
+    }
+}
+
+var all_languages = new Object()
+
+function fill_languages(){
+    
+}
