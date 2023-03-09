882af8aa42d45c66bc0c9d6a9ac83582eb0ed69a
jlostanlen <jeremie.lostanlen@gmail.com>
2023-03-09 20:01:03 +0100

    The array all_currencies is completed !

--- js/Country.js
+++ js/Country.js
@@ -1,3 +1,6 @@
+
+
+
 // This class contains all informations about a country : name, etc.
 class Country {
     constructor(alpha3Code, area, borders, capital, region, demonym, flags, name, population, topLevelDomain, currencies){
@@ -12,23 +15,48 @@ class Country {
         this.population = population
         this.topLevelDomain = topLevelDomain
         this.currencies = currencies
-
     }
 
     toString(){
         return this.name+" have this alpha code 3 : "+this.alpha3Code
     }
+
+    getCurrencies(){
+        console.log(Currency.all_currencies)
+        return Currency.all_currencies 
+    }
 }
+
+
+
+
+
 
 // Function to get informations about each country from the file countries.json
 var all_countries = {}
 
 function fill_db(){
-    for (var element of countries){
-        all_countries[element.alpha3Code] = new Country(element.alpha3Code, element.area, element.borders, element.capital, element.region, element.demonym, element.flags, element.name, element.population, element.topLevelDomain, element.currencies)
-    }
-    console.log(all_countries["AFG"].toString())
+    countries.forEach((co) => { // Iteration by each country
+        
+        // Creation of an array to store each currencies codes of the current country. He will use during the creation of the current country.
+        var all_codes = []
+        
+        // Currency
+        if (!co['currencies']) { // If the country haven't a currency 
+            return
+        }
+        co['currencies'].forEach((cu) => { // Iteration by each currency of a country
+            all_codes.push(cu.code)
+            if(!(cu.code in Currency.all_currencies)) { // If the currency isn't present in the array all_currencies
+                Currency.all_currencies[cu.code] = new Currency(cu.code, cu.name, cu.symbol) // Creation of a new currency and ad it in the array all_currencies
+            }
+        })
 
+        
+        // Creation of a country with his currencies code(s) and his other informations  
+        all_countries[co.alpha3Code] = new Country(co.alpha3Code, co.area, co.borders, co.capital, co.region, co.demonym, co.flags, co.name, co.population, co.topLevelDomain, all_codes)
+    })
 }
+
 
 fill_db()
--- js/Currency.js
+++ js/Currency.js
@@ -1,28 +1,22 @@
 
 class Currency{
-
+    static all_currencies = {}
     constructor(code, name, symbol){
         this.code = code;
         this.name = name;
         this.symbol = symbol;
     }
-
     toString(){
         return this.code + " , " + this.name + " , " + this.symbol;
     }
 }
 
-var all_currency = new Object();
 
-function fillCurrency(){
-    for(let country of Object.values(all_countries)){
-        
-        
-        for(let currency in country.currencies){
-            all_currency[currency.code] = new Currency(currency.code, currency.name, currency.symbol);
-        }
-    }
-}
+// function fillCurrency(){
+//     for(let country of Object.values(all_countries)){
+//         for(let currency in country.currencies){
+//             all_currency[currency.code] = new Currency(currency.code, currency.name, currency.symbol);
+//         }
+//     }
+// }
 
-fillCurrency();
-console.log(all_currency);
