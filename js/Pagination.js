27026ff7d8e4f786771d75d90869821ed1e21369
Louan Robert <louan.robert@dcbrain.com>
2023-03-18 17:28:47 +0100

    adding methods to show more details

--- js/Pagination.js
+++ js/Pagination.js
@@ -1,18 +1,15 @@
-var table = document.getElementsByClassName("datas")[0];
 var page = 1
 const TO_DISPLAY = 25
-var countries = []
+var countriesLoaded = []
 var body = document.getElementsByTagName("body")[0]
-var tbody = document.getElementsByClassName("list_countries")[0]
+var countries = document.getElementById("countries")
 var btSuivant = document.getElementById("suivant")
 var btPrecedent = document.getElementById("precedent")
 
-
-for (var country of Object.values(Country.all_countries)){
-    countries.push(country);
-}
-
 function load(){
+    for (var country of Object.values(Country.all_countries)){
+        countriesLoaded.push(country);
+    }
     btPrecedent.disabled = "true"
     displayData()
 } 
@@ -21,73 +18,80 @@ function load(){
 function displayData(){
     var loop = (page - 1 ) * TO_DISPLAY
 
-    while (loop < TO_DISPLAY * page && loop < countries.length){
+    while (loop < TO_DISPLAY * page && loop < countriesLoaded.length){
+        console.log(loop)
+        var list_countries = document.createElement("article")
+        list_countries.setAttribute("class", "list_countries")
+    
         var div1 = document.createElement("div")
         var nameAndCode = document.createElement("div")
-
+    
         // NAME
         var countryName = document.createElement("p")
-        countryName.textContent = countries[loop].name
+        countryName.textContent = countriesLoaded[loop].name
         nameAndCode.appendChild(countryName)
-
+    
         // CODE
         var countryCode = document.createElement("p")
-        countryCode.textContent = countries[loop].alpha3Code
+        countryCode.textContent = countriesLoaded[loop].alpha3Code
         nameAndCode.appendChild(countryCode)
-
+    
         div1.appendChild(nameAndCode)
-
+        
+    
         // FLAG
         var countryFlag = document.createElement("div")
         var countryFlagImage = document.createElement("img")
-
-        countryFlagImage.src = countries[loop].flag
+        countryFlagImage.src = countriesLoaded[loop].flag
         countryFlagImage.style.height = "100px"
         countryFlag.appendChild(countryFlagImage)
         div1.appendChild(countryFlag)
-
-        var line = document.createElement("ul")
-
-
+    
+        list_countries.appendChild(div1)
+    
+        var datas = document.createElement("ul")
+    
+    
         // POPULATION
         var countryPop = document.createElement("li")
-        countryPop.textContent = countries[loop].population
-        line.appendChild(countryPop)
+        countryPop.textContent = countriesLoaded[loop].population
+        datas.appendChild(countryPop)
         // AREA
         var CountryArea = document.createElement("li")
-        CountryArea.textContent = countries[loop].area
-        line.appendChild(CountryArea)
+        CountryArea.textContent = countriesLoaded[loop].area
+        datas.appendChild(CountryArea)
         // DENSITY
         var countryDensity = document.createElement("li")
-        countryDensity.textContent = countries[loop].getPopDensity()
-        line.appendChild(countryDensity)
+        countryDensity.textContent = countriesLoaded[loop].getPopDensity()
+        datas.appendChild(countryDensity)
         // REGION
         var countryRegion = document.createElement("li")
-        countryRegion.textContent = countries[loop].region
-        line.appendChild(countryRegion)
-
-        div1.appendChild(line)
+        countryRegion.textContent = countriesLoaded[loop].region
+        datas.appendChild(countryRegion)
+    
+        list_countries.appendChild(datas)
         
-
+    
         // Add lines to tab
-        tbody.appendChild(div1) 
+        countries.appendChild(list_countries) 
         loop++;
     }
 }
 
 btSuivant.addEventListener("click", function(){
     console.log("cliqué")
-    table.innerHTML=""
+    countries.innerHTML=""
     btPrecedent.disabled = false
     page++;
-    (page*TO_DISPLAY >= countries.length)? btSuivant.disabled = true :  btSuivant.disabled = false
+    (page*TO_DISPLAY >= countriesLoaded.length)? btSuivant.disabled = true :  btSuivant.disabled = false;
     displayData();
 })
 
 btPrecedent.addEventListener("click", function(){
     console.log("cliqué")
-    table.innerHTML=""
+    countries.innerHTML=""
     page--;
-    (page == 1)? btPrecedent.disabled = true :  btPrecedent.disabled = false
+    (page == 1)? btPrecedent.disabled = true :  btPrecedent.disabled = false;
+    (page*TO_DISPLAY >= countriesLoaded.length)? btSuivant.disabled = true :  btSuivant.disabled = false;
     displayData()
 })
--- js/moreInfos.js
+++ js/moreInfos.js
@@ -1,105 +1,163 @@
-var table = document.getElementsByClassName("datas")[0];
 var page = 1
 const TO_DISPLAY = 25
-var countries = []
+var countriesLoaded = []
 var body = document.getElementsByTagName("body")[0]
-var tbody = document.getElementsByClassName("datas")[0]
+var countries = document.getElementById("countries")
 var btSuivant = document.getElementById("suivant")
 var btPrecedent = document.getElementById("precedent")
-var moreInfo = document.getElementById("moreInfo")
-var moreInfoDisplay = document.getElementById("moreInfoDisplay")
+var btCloseInfos = document.getElementById("closeInfos")
+var btCloseFlag = document.getElementById("closeFlag")
 
+var moreInfos = document.getElementById("moreInfos")
+var moreInfosPlaceHolder = document.getElementById("moreInfosPlaceHolder")
+var btMoreInfo= []
 
-
-for (var country of Object.values(Country.all_countries)){
-    countries.push(country);
-}
 
 function load(){
+    for (var country of Object.values(Country.all_countries)){
+        countriesLoaded.push(country);
+    }
     btPrecedent.disabled = "true"
     displayData()
 } 
 
-function addListeners(){
-    var tableRows = document.getElementsByClassName("data");
-    console.log(moreInfoDisplay)
-    for (element of tableRows) {
-        element.addEventListener("click", () =>{
-            var id = element.id;
-            for (var attribute of Object.values(Country.all_countries[id])){
-                var line = document.createElement("tr");
-                var column = document.createElement("td")
-                column.textContent = attribute
-                line.appendChild(column)
-                moreInfoDisplay.appendChild(line)
-            }
-            
-            moreInfo.style.display="block";
-            document.getElementsByTagName("main")[0].style.filter = "blur(5px)"
-        })
-    }
-}
 
 function displayData(){
     var loop = (page - 1 ) * TO_DISPLAY
-
-    while (loop < TO_DISPLAY * page && loop < countries.length){
-        console.log(loop)
-        var line = document.createElement("tr")
-        line.setAttribute("id", countries[loop].alpha3Code);
-        line.setAttribute("class", "data");
-
-
+    
+    while (loop < TO_DISPLAY * page && loop < countriesLoaded.length){
+        var list_countries = document.createElement("article")
+        list_countries.setAttribute("class", "list_countries")
+        
+    
+        var div1 = document.createElement("div")
+        var nameAndCode = document.createElement("div")
+    
         // NAME
-        var countryName = document.createElement("td")
-        countryName.textContent = countries[loop].name
-        line.appendChild(countryName)
-        // POPULATION
-        var countryPop = document.createElement("td")
-        countryPop.textContent = countries[loop].population
-        line.appendChild(countryPop)
-        // AREA
-        var CountryArea = document.createElement("td")
-        CountryArea.textContent = countries[loop].area
-        line.appendChild(CountryArea)
-        // DENSITY
-        var countryDensity = document.createElement("td")
-        countryDensity.textContent = countries[loop].getPopDensity()
-        line.appendChild(countryDensity)
-        // REGION
-        var countryRegion = document.createElement("td")
-        countryRegion.textContent = countries[loop].region
-        line.appendChild(countryRegion)
+        var countryName = document.createElement("p")
+        countryName.textContent = countriesLoaded[loop].name
+        nameAndCode.appendChild(countryName)
+    
+        // CODE
+        var countryCode = document.createElement("p")
+        countryCode.textContent = countriesLoaded[loop].alpha3Code
+        nameAndCode.appendChild(countryCode)
+    
+        div1.appendChild(nameAndCode)
+        
+    
         // FLAG
-        var countryFlag = document.createElement("td")
+        var countryFlag = document.createElement("div")
         var countryFlagImage = document.createElement("img")
-        countryFlagImage.src = countries[loop].flag
+        countryFlagImage.src = countriesLoaded[loop].flag
         countryFlagImage.style.height = "100px"
-        //countryFlag.textContent = country.flag
         countryFlag.appendChild(countryFlagImage)
-        line.appendChild(countryFlag)
+        countryFlag.setAttribute("flag", countriesLoaded[loop].flag)
+        countryFlag.setAttribute("onclick", "biggerFlag(this)")
+        div1.appendChild(countryFlag)
+    
+        list_countries.appendChild(div1)
+    
+        var datas = document.createElement("ul")
+    
+    
+        // POPULATION
+        var countryPop = document.createElement("li")
+        countryPop.textContent = countriesLoaded[loop].population
+        datas.appendChild(countryPop)
+        // AREA
+        var CountryArea = document.createElement("li")
+        CountryArea.textContent = countriesLoaded[loop].area
+        datas.appendChild(CountryArea)
+        // DENSITY
+        var countryDensity = document.createElement("li")
+        countryDensity.textContent = countriesLoaded[loop].getPopDensity()
+        datas.appendChild(countryDensity)
+        // REGION
+        var countryRegion = document.createElement("li")
+        countryRegion.textContent = countriesLoaded[loop].region
+        datas.appendChild(countryRegion)
+      
+        datas.setAttribute("id", countriesLoaded[loop].alpha3Code)
+        datas.setAttribute("onclick", "displayMoreInfos(this.id)")
+        
+        list_countries.appendChild(datas)
+        
     
         // Add lines to tab
-        table.appendChild(line) 
+        countries.appendChild(list_countries) 
         loop++;
-    }
-    addListeners()
 
+        
+    }
 }
+
+function displayMoreInfos(id){
+    console.log("cliqué")
+    console.log(id)
+
+    moreInfos.innerHTML=""
+    var table = document.createElement("table")
+    
+
+    for (var attribute of Object.keys(Country.all_countries[id])){
+
+        if (attribute != "flag"){
+            var line = document.createElement("tr")
+            var tableHead = document.createElement("th")
+            tableHead.textContent = attribute
+            line.appendChild(tableHead)
+
+
+            var tableData = document.createElement("td")
+            tableData.textContent = Country.all_countries[id][attribute]
+      
+
+            line.appendChild(tableData)
+            table.appendChild(line)
+        }
+        
+    }
+    moreInfos.appendChild(table)
+    moreInfosPlaceHolder.style.display="block";
+    document.getElementsByTagName("main")[0].style.filter = "blur(5px)"
+}
+
 
 btSuivant.addEventListener("click", function(){
     console.log("cliqué")
-    table.innerHTML=""
+    countries.innerHTML=""
     btPrecedent.disabled = false
     page++;
-    (page*TO_DISPLAY >= countries.length)? btSuivant.disabled = true :  btSuivant.disabled = false
+    (page*TO_DISPLAY >= countriesLoaded.length)? btSuivant.disabled = true :  btSuivant.disabled = false;
     displayData();
 })
 
 btPrecedent.addEventListener("click", function(){
     console.log("cliqué")
-    table.innerHTML=""
+    countries.innerHTML=""
     page--;
-    (page == 1)? btPrecedent.disabled = true :  btPrecedent.disabled = false
+    (page == 1)? btPrecedent.disabled = true :  btPrecedent.disabled = false;
+    (page*TO_DISPLAY >= countriesLoaded.length)? btSuivant.disabled = true :  btSuivant.disabled = false;
     displayData()
 })
+
+
+btCloseInfos.addEventListener("click", ()=>{
+    console.log("cliqué")
+    moreInfosPlaceHolder.style.display = "none"
+    document.getElementsByTagName("main")[0].style.filter = "blur(0px)"
+})
+
+function biggerFlag(source){
+    
+    moreInfos.innerHTML=""
+    var flag = document.createElement("img");
+    flag.src = source.getAttribute("flag");
+    flag.style.height="500px"
+    
+    moreInfos.appendChild(flag)
+    document.getElementsByTagName("main")[0].style.filter = "blur(5px)"
+    moreInfosPlaceHolder.style.display="block"
+
+}
